cbc4b8e219286f7ffeb1c7180374978b8e61a27d
DokyungYou <willow74rr@gmail.com>
2024-06-18 16:13:53 +0900

    여러가지연산자2
    - 비트 연산자
      - 일단 후순위로 두었음

---
+++ src/grammer/여러가지연산자/Main2.java
@@ -0,0 +1,36 @@
+package grammer.여러가지연산자;
+
+// 비트 연산자는 나중에
+public class Main2 {
+
+    public static void main(String[] args) {
+
+//      1. 비트 논리 연산자
+        System.out.println("== 비트 논리 연산자 ==");
+//      1-1. AND 연산자 (&)
+
+
+//      1-2. OR 연산자 (|)
+
+
+//      1-3. XOR 연산자 (^)
+
+
+//      1-4. 반전 연산자 (~)
+
+
+
+//      2. 비트 이동 연산자
+        System.out.println("== 비트 이동 연산자 ==");
+//      2-1. << 연산자
+
+
+//      2-2. >> 연산자
+
+
+//      2-3. >>> 연산자
+
+
+    }
+
+}
