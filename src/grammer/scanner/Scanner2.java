1a5218d4f0940efaea2acd9fb02e67fdde83b1d5
DokyungYou <willow74rr@gmail.com>
2024-06-19 23:27:50 +0900

    Scanner 실습

---
+++ src/grammer/scanner/Scanner2.java
@@ -0,0 +1,22 @@
+package grammer.scanner;
+
+import java.util.Scanner;
+
+public class Scanner2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫번째 숫자를 입력하시오.");
+        int num1 = scanner.nextInt();
+
+        System.out.println("두번째 숫자를 입력하시오.");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2 ;
+        System.out.println("두 수의 합은 "+ sum + "입니다.");
+
+        scanner.close();
+
+
+
+    }
+}
