02e17e17c319e940fddcddb84158077f414ac5af
DokyungYou <willow74rr@gmail.com>
2024-06-20 14:47:11 +0900

    array 예제7 실습

---
+++ src/grammer/array/Example/Example7.java
@@ -0,0 +1,11 @@
+package grammer.array.Example;
+
+import java.util.Scanner;
+
+public class Example7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
+    }
+}
