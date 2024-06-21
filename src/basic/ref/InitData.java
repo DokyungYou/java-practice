0585b1fa9586c65932570b982cb6cb58fe8e5583
DokyungYou <willow74rr@gmail.com>
2024-06-21 19:19:27 +0900

    ref 실습3 - 초기화 - 기본타입변수 초기화 - 인스턴스 변수 초기화

---
+++ src/basic/ref/InitData.java
@@ -0,0 +1,26 @@
+package basic.ref;
+
+public class InitData {
+    int num;
+    String str;
+    boolean isTrue;
+
+    public static void main(String[] args) {
+
+
+        // int a;
+        // System.out.println(a);
+        // 기본 자료형은 직접 초기화를 해줘야함 (에러 뜸)
+
+
+        InitData data = new InitData();
+        
+        // 인스턴스의 멤버 변수는 인스턴스 생성 시 자동 초기화
+        System.out.println(data.num);  // 0
+        System.out.println(data.str);  // null
+        System.out.println(data.isTrue); // boolean
+
+        
+
+    }
+}
