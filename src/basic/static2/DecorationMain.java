7275b93d190115fd2b4706dbc6fb02756eb8ad28
DokyungYou <willow74rr@gmail.com>
2024-06-24 00:14:32 +0900

    static import

--- src/basic/static2/DecorationMain.java
+++ src/basic/static2/DecorationMain.java
@@ -1,5 +1,7 @@
 package basic.static2;
 
+import static basic.static2.DecorationData.staticMethod;
+
 public class DecorationMain {
     public static void main(String[] args) {
         String str = "Java";
@@ -19,5 +21,20 @@ public class DecorationMain {
         System.out.println("== instance 호출 02 ==");
         DecorationData data2 = new DecorationData();
         data2.instanceMethod();  // instance: 1, static: 3
+
+        // ===================================================
+
+
+        DecorationData.staticMethod();
+
+        // 이런 식으로 한 파일 내에서 static요소를  빈번하게 호출 시 static import 고려
+        // ex) import static basic.static2.DecorationData.staticMethod;  // 특정 메소드가 아닌 전체를 가져오고싶다면 .*
+        staticMethod();
+        staticMethod();
+        staticMethod();
+        staticMethod();
+
+
+
     }
 }
