14bf304ce0d4fb8678561ac945f2f7e0c3f7828c
DokyungYou <willow74rr@gmail.com>
2024-06-23 11:17:57 +0900

    import *  주석내용 수정

--- src/basic/pack/package1/Package1Main.java
+++ src/basic/pack/package1/Package1Main.java
@@ -3,7 +3,7 @@ package basic.pack.package1;
 //import basic.pack.Data;
 //import basic.pack.Member;
 
-//  basic.pack 내의 모든 것 import
+//  basic.pack 내의 모든 파일 import (하위 패키지는 포함 X)
 import basic.pack.*;
 
 public class Package1Main {
