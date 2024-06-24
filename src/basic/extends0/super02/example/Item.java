b6c18b23e54939eb025882d919a5bb58c7f4e443
DokyungYou <willow74rr@gmail.com>
2024-06-25 00:01:13 +0900

    super 클래스, 메서드에 final사용
    - 클래스 final
      - 상속 끝
    - 메서드 final
      - 오버라이드 끝

--- src/basic/extends0/super02/example/Item.java
+++ src/basic/extends0/super02/example/Item.java
@@ -1,5 +1,8 @@
 package basic.extends0.super02.example;
 
+
+// public final class Item
+// 이렇게 클래스에 final 을 붙일 수 있는데 그럼 상속못해줌
 public class Item {
     // 매번 캡슐화해야한다는걸 까먹는다..
     private String name;
@@ -12,6 +15,10 @@ public class Item {
 
     public void showInfo(){
         System.out.println("이름: "+ name+ " 가격: "+ price);
+    }
+
+    public final void cannotOverrideMethod(){
+        System.out.println("이건 자식이어도 못 준다.");
     }
 
 }
