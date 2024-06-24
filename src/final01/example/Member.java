3cf4b739e822d35b6725361adba8d3170323ceb1
DokyungYou <willow74rr@gmail.com>
2024-06-24 19:24:40 +0900

    final 예제

---
+++ src/final01/example/Member.java
@@ -0,0 +1,21 @@
+package final01.example;
+
+public class Member {
+
+    private final String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void changeMemberInfo(String name){
+        // this.id = id;
+        this.name = name;
+    }
+
+    public void printMemberInfo(){
+        System.out.println("id: " + id+ ", name: "+ name);
+    }
+}
---
+++ src/final01/example/MemberMain.java
@@ -0,0 +1,9 @@
+package final01.example;
+
+public class MemberMain {
+    public static void main(String[] args) {
+        Member member1 = new Member("javaKim01","김자바");
+        member1.printMemberInfo();
+
+    }
+}
