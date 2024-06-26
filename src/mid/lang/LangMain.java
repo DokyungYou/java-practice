75070cdb131971748f6c97752fefdd57d29a530d
DokyungYou <willow74rr@gmail.com>
2024-06-27 00:24:23 +0900

    Object 클래스

---
+++ src/mid/lang/LangMain.java
@@ -0,0 +1,16 @@
+package mid.lang;
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("System 클래스는 Lang 패키지에 속한다.");
+        
+        Object str = "String 클래스";
+        System.out.println(str.toString());
+
+        /* Object의 toString()
+        *  public String toString() {
+            return getClass().getName() + "@" + Integer.toHexString(hashCode());
+         }
+        * */
+    }
+}
---
+++ src/mid/lang/object/ObjectMain.java
@@ -0,0 +1,24 @@
+package mid.lang.object;
+
+public class ObjectMain {
+    public static void main(String[] args) {
+        
+        // Child 본인과 명시적 상속받은 기능 호출 ====================================
+        Child child = new Child();
+        child.childMethod();
+        child.parentMethod();
+
+        
+        // 묵시적 상속받은 기능 호출 (Object 클래스) ================================
+        // 대표 기능 예시
+        String str = child.toString(); // 객체정보를 제공
+        System.out.println(str);
+        
+        boolean equals = child.equals(new Child()); // 객체의 같음을 비교
+        System.out.println(equals);
+
+        Class<? extends Child> childClass = child.getClass(); // 객체의 클래스 정보를 제공
+
+
+    }
+}
