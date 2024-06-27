0eb161ef2f28702270dc36821ef474397940398d
DokyungYou <willow74rr@gmail.com>
2024-06-28 02:20:33 +0900

    예제1 - startsWith

---
+++ src/mid/lang/string/example/Example01.java
@@ -0,0 +1,12 @@
+package mid.lang.string.example;
+
+public class Example01 {
+    public static void main(String[] args) {
+
+        // https:// 로 시작하는가?
+        String url = "https://github.com/java";
+        System.out.println(url.startsWith("https://"));
+
+
+    }
+}
