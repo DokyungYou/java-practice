e93ade5b8f1929cf518afa4690971ccd725e8902
DokyungYou <willow74rr@gmail.com>
2024-06-27 22:33:25 +0900

    String 클래스 - 주요 메서드1

---
+++ src/mid/lang/string/method/StringInfoMain.java
@@ -0,0 +1,16 @@
+package mid.lang.string.method;
+
+public class StringInfoMain {
+    public static void main(String[] args) {
+        String str = "Java Kim";
+        System.out.println("문자열의 길이: " + str.length()); // 공백문자도 길이에 포함된다.
+        System.out.println("문자열이 비어있는지: " + str.isEmpty()); // return value.length == 0;
+
+        System.out.println("문자열이 비어있거나 공백인지: " + str.isBlank());  // false
+        System.out.println("문자열이 비어있거나 공백인지: " + "  ".isBlank()); // true
+
+        char char0 = str.charAt(0);
+        System.out.println("char0: " + char0);  // J
+        System.out.println("char4: " + str.charAt(4));  // " "
+    }
+}
