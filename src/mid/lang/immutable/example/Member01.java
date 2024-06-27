b024c3c05c77667d31f696196e3439e5818de0c8
DokyungYou <willow74rr@gmail.com>
2024-06-27 17:45:02 +0900

    불변객체 예제

---
+++ src/mid/lang/immutable/example/Member01.java
@@ -0,0 +1,37 @@
+package mid.lang.immutable.example;
+
+import mid.lang.immutable.address.Address;
+
+public class Member01 {
+    private String name;
+    private Address address;
+
+    public Member01(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/mid/lang/immutable/example/Member01Main.java
@@ -0,0 +1,25 @@
+package mid.lang.immutable.example;
+
+import mid.lang.immutable.address.Address;
+
+public class Member01Main {
+    public static void main(String[] args) {
+        Address address1 = new Address("서울");
+
+        // 주소가 서울인 회원이 두명이고, 메모리 효율을 위해 같은 주소 객체로 값을 할당해주었다.
+        Member01 member01 = new Member01("가나다",address1);
+        Member01 member02 = new Member01("마바사",address1);
+        
+        //  두 회원의 첫 주소는 서울
+        System.out.println("member1: " + member01);
+        System.out.println("member2:  " + member02);
+
+        // 회원2만 인천으로 이사를 했다.
+        member02.getAddress().setValue("인천");
+
+        // 회원1도 강제 이사당해버렸다... (address 공유참조로 인한 사이드이펙트발생)
+        System.out.println("member1: " + member01);
+        System.out.println("member2:  " + member02);
+
+    }
+}
