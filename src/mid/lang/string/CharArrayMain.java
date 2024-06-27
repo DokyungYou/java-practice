44ea9b917bab69df905b0346474dcac00d74f0b3
DokyungYou <willow74rr@gmail.com>
2024-06-27 20:51:24 +0900

    String 클래스 - 기본

---
+++ src/mid/lang/string/CharArrayMain.java
@@ -0,0 +1,11 @@
+package mid.lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char[] charArr = {'j','a','v','a','K','i','m'};
+        System.out.println(charArr);
+
+        String str = "javaKim";
+        System.out.println(str);
+    }
+}
