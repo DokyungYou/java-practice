b57d1f3c25c9a8fd0ce940526771d1526081bfd3
DokyungYou <willow74rr@gmail.com>
2024-06-27 18:40:51 +0900

    불변객체 예제 - 값 변경

---
+++ src/mid/lang/immutable/change/MutableObject.java
@@ -0,0 +1,22 @@
+package mid.lang.immutable.change;
+
+public class MutableObject {
+
+    private int value;
+
+    public MutableObject(int value){
+        this.value = value;
+    }
+
+    public void add(int addValue){
+        value += addValue;  // 속성값이 변함 => MutableObject 는 불변객체가 아니다.
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
