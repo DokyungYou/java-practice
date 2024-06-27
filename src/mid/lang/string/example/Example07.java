4f082f68372339bbdc2ebfe1925ac6dac05487c4
DokyungYou <willow74rr@gmail.com>
2024-06-28 03:32:04 +0900

    예제7 - replace

---
+++ src/mid/lang/string/example/Example07.java
@@ -0,0 +1,10 @@
+package mid.lang.string.example;
+
+public class Example07 {
+    public static void main(String[] args) {
+        // Java -> Spring
+        String str = "Great JavaKim Great JavaKim Great JavaKim Great JavaKim ";
+        System.out.println(str.replace("Java", "Spring"));
+
+    }
+}
