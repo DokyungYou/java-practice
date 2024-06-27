5810c93f94164a1db3eec60c9d0542bc64cbc763
DokyungYou <willow74rr@gmail.com>
2024-06-27 15:57:01 +0900

    기본형 & 참조형 공유

---
+++ src/mid/lang/immutable/address/RefMain1.java
@@ -0,0 +1,19 @@
+package mid.lang.immutable.address;
+
+public class RefMain1 {
+    public static void main(String[] args) {
+        
+        //참조형 변수는 하나의 인스턴스를 공유할 수 있음
+        Address address1 = new Address("주소1");
+        Address address2 = address1;
+
+        System.out.println("address1: " + address1);
+        System.out.println("address2: " + address2);
+
+
+        System.out.println("주소2 -> address2");
+        address2.setValue("주소2");
+        System.out.println("address1: " + address1);
+        System.out.println("address2: " + address2);
+    }
+}
