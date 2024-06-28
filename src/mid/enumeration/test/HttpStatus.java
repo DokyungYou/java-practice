3498b1b01b0f94387a6e8f89ee7c87cda1819b80
DokyungYou <willow74rr@gmail.com>
2024-06-29 04:39:34 +0900

    Enum - 문제3
    - HttpStatus

---
+++ src/mid/enumeration/test/HttpStatus.java
@@ -0,0 +1,48 @@
+package mid.enumeration.test;
+
+public enum HttpStatus {
+    OK(200,"OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error")
+    ;
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public static HttpStatus getByCode(int code){
+        for(HttpStatus status : HttpStatus.values()){
+//            if(code >= 200 && code < 300){
+//                return HttpStatus.OK;
+//            }
+
+            if(status.getCode() == code){
+                return status;
+            }
+        }
+        return null;
+    }
+
+    public boolean isSuccess(){
+        return (code == 200);
+    }
+
+    public void printStatus(){
+        System.out.println("HTTP CODE: " + code);
+        System.out.println(code + " " + message);
+        System.out.println("isSuccess: " + isSuccess());
+    }
+}
---
+++ src/mid/enumeration/test/HttpStatusScanner.java
@@ -0,0 +1,22 @@
+package mid.enumeration.test;
+
+import java.util.Scanner;
+
+public class HttpStatusScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.print("HTTP CODE: ");
+            HttpStatus httpStatus = HttpStatus.getByCode(scanner.nextInt());
+            if(httpStatus == null){
+                System.out.println("정의되지 않은 코드입니다.");
+                continue;
+            }
+            httpStatus.printStatus();
+            break;
+        }
+
+        scanner.close();
+    }
+}
