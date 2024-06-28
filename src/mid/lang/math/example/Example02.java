3e5227a0f40ef0a10e974b8bf2473e70b6b7784a
DokyungYou <willow74rr@gmail.com>
2024-06-28 20:24:55 +0900

    예제2

---
+++ src/mid/lang/math/example/Example02.java
@@ -0,0 +1,16 @@
+package mid.lang.math.example;
+
+public class Example02 {
+    public static void main(String[] args) {
+        // 배열에 입력된 모든 숫자의 합 구하기
+        String[] numbers = {"1.2", "4", "3", "-1", "-10", "4.4", "7", "9.9"};
+
+        double result = 0.0;
+        for(String num : numbers){
+            double parseNum = Double.parseDouble(num);
+            result += parseNum;
+            System.out.print(parseNum + "  ");
+        }
+        System.out.println("\n result: " + result);
+    }
+}
