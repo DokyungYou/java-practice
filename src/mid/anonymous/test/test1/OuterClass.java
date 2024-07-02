87ac345c2f98c933234bb9000c742953816af40f
DokyungYou <willow74rr@gmail.com>
2024-07-02 18:43:13 +0900

    문제1 - 정적 중첩 클래스 완성하기

---
+++ src/mid/anonymous/test/test1/OuterClass.java
@@ -0,0 +1,12 @@
+package mid.anonymous.test.test1;
+
+import java.util.Arrays;
+
+public class OuterClass {
+
+    static class NestedClass{
+        public void printMethod(){
+            System.out.println(Arrays.toString(NestedClass.class.getDeclaredMethods()));
+        }
+    }
+}
---
+++ src/mid/anonymous/test/test1/OuterClassMain.java
@@ -0,0 +1,8 @@
+package mid.anonymous.test.test1;
+
+public class OuterClassMain {
+    public static void main(String[] args) {
+        OuterClass.NestedClass nestedClass = new OuterClass.NestedClass();
+        nestedClass.printMethod();
+    }
+}
