17dd4215b9161e8c82cd37f50071b809da36ccfe
DokyungYou <willow74rr@gmail.com>
2024-07-02 18:22:43 +0900

    익명클래스 활용 - 지역클래스 리팩토링

---
+++ src/mid/anonymous/example/ref2/Refactoring02_2.java
@@ -0,0 +1,37 @@
+package mid.anonymous.example.ref2;
+
+import mid.anonymous.example.Processor;
+
+import java.util.Random;
+
+public class Refactoring02_2 {
+
+    public static void hello(Processor processor){
+        System.out.println("program start");
+        processor.run();
+        System.out.println("program end");
+    }
+
+    public static void main(String[] args) {
+
+        // 일회용일 경우 익명클래스로 구현
+
+        hello(new Processor() {
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위 결과: " + randomValue);
+            }
+        });
+        
+        hello(new Processor() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        });
+    }
+
+}
