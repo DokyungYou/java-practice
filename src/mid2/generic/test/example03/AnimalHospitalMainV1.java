d2e23a64931ee0bb3e36d4023bb9d9e0dd505f07
DokyungYou <willow74rr@gmail.com>
2024-07-04 16:54:14 +0900

    제네릭 기본예제3(타입매개변수 제한2) - 다형성 시도
    - 코드 재사용성 O
    - 타입 안정성 떨어짐

---
+++ src/mid2/generic/test/example03/AnimalHospitalMainV1.java
@@ -0,0 +1,22 @@
+package mid2.generic.test.example03;
+
+import mid2.generic.animal.Animal;
+import mid2.generic.animal.Cat;
+import mid2.generic.animal.Tiger;
+
+public class AnimalHospitalMainV1 {
+    public static void main(String[] args) {
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
+        catHospital.setAnimal(new Cat("고영희", 100));
+        catHospital.setAnimal(new Tiger("호냥이", 1000)); // 의도는 Cat만 들어가야하지만 Tiger도 허용됨
+        catHospital.checkup();
+
+
+        // 의도는 Cat만 들어가야하지만 Tiger도 허용됨
+        // 반환타입 Cat이 아닌 Animal (다운캐스팅 필요) -> Tiger이 더 큼 -> Tiger타입 반환 -> 캐스팅예외발생
+        Cat bigger = (Cat)(catHospital.bigger(new Cat("메인쿤", 500)));
+        //Cat bigger2 = (Cat)(catHospital.bigger(new Tiger("호냥이", 1000)));
+        System.out.println("더 큰 고양이: " + bigger);
+
+    }
+}
---
+++ src/mid2/generic/test/example03/AnimalHospitalV1.java
@@ -0,0 +1,22 @@
+package mid2.generic.test.example03;
+
+import mid2.generic.animal.Animal;
+import mid2.generic.animal.Tiger;
+
+public class AnimalHospitalV1 {
+    private Animal animal;
+    public void setAnimal(Animal animal){
+        this.animal = animal;
+    }
+
+    public void checkup(){
+        System.out.println("동물이름: " + animal.getName());
+        System.out.println("동물크기: " + animal.getSize());
+        animal.sound();
+    }
+
+    public Animal bigger(Animal targetAnimal){
+        return animal.getSize() > targetAnimal.getSize() ? animal : targetAnimal;
+    }
+
+}
