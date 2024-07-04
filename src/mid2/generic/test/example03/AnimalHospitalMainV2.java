c1c9575643204ee1b97a6856e96b2cb4dc6a5771
DokyungYou <willow74rr@gmail.com>
2024-07-04 18:33:23 +0900

    제네릭 기본예제3(타입매개변수 제한3) - 제네릭 도입 실패

---
+++ src/mid2/generic/test/example03/AnimalHospitalMainV2.java
@@ -0,0 +1,17 @@
+package mid2.generic.test.example03;
+
+import mid2.generic.animal.Animal;
+import mid2.generic.animal.Cat;
+import mid2.generic.animal.Tiger;
+
+public class AnimalHospitalMainV2 {
+    public static void main(String[] args) {
+
+        // Animal과는 관련없는 타입들도 모두 타입인수로 넘겨버릴 수 있는 상황
+        AnimalHospitalV2<Cat> hospital1 = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Tiger> hospital2 = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Long> hospital3 = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Object> hospital4 = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Animal> hospital5 = new AnimalHospitalV2<>();
+    }
+}
---
+++ src/mid2/generic/test/example03/AnimalHospitalV2.java
@@ -0,0 +1,22 @@
+package mid2.generic.test.example03;
+
+public class AnimalHospitalV2<T>{
+    private T animal;
+    public void setAnimal(T animal){
+        this.animal = animal;
+    }
+
+    public void checkup(){
+        
+        // T의 타입은 객체 생성전까지는 알 수 없기때문에 Object의 메서드만 사용가능
+//        System.out.println("동물이름: " + animal.getName());
+//        System.out.println("동물크기: " + animal.getSize());
+//        animal.sound();
+    }
+
+    public T bigger(T targetAnimal){
+        //return animal.getSize() > targetAnimal.getSize() ? animal : targetAnimal;
+        return null;
+    }
+
+}
