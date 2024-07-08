1e09a50d0ee18ca69e0724fd76e3dbe80dad3127
DokyungYou <willow74rr@gmail.com>
2024-07-08 23:37:03 +0900

    직접 구현하는 Set (version1)

---
+++ src/mid2/collection/set/MyHashSetV1.java
@@ -0,0 +1,69 @@
+package mid2.collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV1 {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 15;
+    private LinkedList<Integer>[] elements;
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV1(){
+        initialList(capacity);
+    }
+
+    public MyHashSetV1(int capacity){
+        initialList(capacity);
+
+    }
+
+    private void initialList(int capacity){
+        elements = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            elements[i] = new LinkedList<>();
+        }
+    }
+
+
+    public boolean add(int value){
+        if(contains(value)) return false;
+        elements[getHashIndex(value)].add(value);
+        size++;
+        return true;
+    }
+
+    public boolean remove(int value){
+
+        boolean isRemoved = elements[getHashIndex(value)].remove(Integer.valueOf(value)); // remove(int index) , remove(Object o)
+        if(!isRemoved) return false;
+        size--;
+        return true;
+    }
+
+    private int getHashIndex(int value){
+        int hashIndex = value % elements.length;
+        if(value < 0) return hashIndex * -1;
+
+        return hashIndex;
+    }
+
+    public boolean contains(int value) { // O(n)
+        LinkedList<Integer> list = elements[getHashIndex(value)];
+        return list.contains(value);
+    }
+
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "elements=" + Arrays.toString(elements) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
---
+++ src/mid2/collection/set/MyHashSetV1Main.java
@@ -0,0 +1,24 @@
+package mid2.collection.set;
+
+public class MyHashSetV1Main {
+    public static void main(String[] args) {
+        MyHashSetV1 myHashset = new MyHashSetV1(20);
+        myHashset.add(1);
+        myHashset.add(6);
+        myHashset.add(2);
+        myHashset.add(3);
+        myHashset.add(11);
+        myHashset.add(-12);
+        myHashset.add(99);
+        myHashset.add(-99);
+        System.out.println(myHashset);
+
+        System.out.println("myHashset.contains(99): " + myHashset.contains(-99));
+        System.out.println("myHashset.contains(1): " + myHashset.contains(-1));
+
+        System.out.println("myHashset.remove(11): " + myHashset.remove(11));
+        System.out.println("myHashset.remove(15): " + myHashset.remove(15));
+        System.out.println("myHashset.remove(-99): " + myHashset.remove(-99));
+        System.out.println(myHashset);
+    }
+}
