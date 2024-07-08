db54baca35ea08943df538a611bf62afa5b86e03
DokyungYou <willow74rr@gmail.com>
2024-07-08 18:59:37 +0900

    해시알고리즘1
    - 데이터 추가 시의 성능문제

---
+++ src/mid2/collection/set/HashStart01.java
@@ -0,0 +1,22 @@
+package mid2.collection.set;
+
+import java.util.Arrays;
+
+public class HashStart01 {
+    public static void main(String[] args) {
+        Integer[] inputArray = new Integer[5];
+        inputArray[0] =1;
+        inputArray[1] =3;
+        inputArray[2] =5;
+        inputArray[3] =7;
+        inputArray[4] =9;
+        System.out.println(Arrays.toString(inputArray));
+        
+        int searchValue = 9;
+        for (Integer value : inputArray) { // O(n)
+            if(value == searchValue) System.out.println(searchValue);
+
+        }
+
+    }
+}
