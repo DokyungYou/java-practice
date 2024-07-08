ba46c5b4f92f34d66793ef9194e181a2ad12db73
DokyungYou <willow74rr@gmail.com>
2024-07-08 18:41:44 +0900

    직접 구현하는 Set (version0)

---
+++ src/mid2/collection/set/MyHashSetV0.java
@@ -0,0 +1,36 @@
+package mid2.collection.set;
+
+import java.util.Arrays;
+
+public class MyHashSetV0 {
+    private int[] elements = new int[10];
+    private int size = 0;
+
+
+    // 데이터를 추가할때마다 O(n) 의 성능 O(1) * O(n))
+    public boolean add(int value){
+        if(contains(value)) return false; // O(n)
+
+        elements[size++] = value; // O(1)
+        return true;
+    }
+
+    public boolean contains(int value) { // O(n)
+        for (int i = 0; i < size; i++) {
+            if(value == elements[i]) return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV0{" +
+                "element=" + Arrays.toString(Arrays.copyOf(elements, size)) +
+                ", size=" + size +
+                '}';
+    }
+}
---
+++ src/mid2/collection/set/MyHashSetV0Main.java
@@ -0,0 +1,19 @@
+package mid2.collection.set;
+
+public class MyHashSetV0Main {
+    public static void main(String[] args) {
+        MyHashSetV0 myHashSet = new MyHashSetV0();
+        myHashSet.add(1); // O(1)
+        myHashSet.add(2); // O(1) * O(n)
+        myHashSet.add(3); // O(1) * O(n)
+        myHashSet.add(4); // O(1) * O(n)
+        System.out.println(myHashSet);
+
+        System.out.println("중복 데이터 저장시도: " + myHashSet.add(4));
+        System.out.println(myHashSet);
+
+        System.out.println("myHashSet.contains(3): " + myHashSet.contains(3));
+        System.out.println("myHashSet.contains(0): " + myHashSet.contains(0));
+        System.out.println(myHashSet);
+    }
+}
