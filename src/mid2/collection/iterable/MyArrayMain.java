6731a20f1a39ce89020235ccae283631a3b170fc
DokyungYou <willow74rr@gmail.com>
2024-07-11 14:21:49 +0900

    순회2 - 향상된 for문

--- src/mid2/collection/iterable/MyArrayMain.java
+++ src/mid2/collection/iterable/MyArrayMain.java
@@ -10,5 +10,11 @@ public class MyArrayMain {
         while (iterator.hasNext()){
             System.out.println("value: " + iterator.next());
         }
+
+        // for-each 문 (향상된 for문)
+        // 컴파일 시점에 위의 iterator 반복문 코드와 같이 변경되는 것이다.
+        for (int value : myArray) {
+            System.out.println("value: " + value);
+        }
     }
 }
